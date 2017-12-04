2362bc65f4651dab9de83591f2bb6ac38b7b4ef7
Jawann <jawann@gmail.com>
2017-12-04 04:16:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name, :songs
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -9,10 +10,15 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(title)
     song = Song.new(title)
     self.add_song(song)
+  end
+
+  def self.song_count
+    @@song_count.length
   end
 end
